5d6d544af04c5ba7d286a68768866f7ceacac3c9
Kyle Coberly <kyle.coberly@gmail.com>
2019-01-15 11:58:25 -0700

    FIxing typo

--- utilities/build-url.js
+++ utilities/build-url.js
@@ -32,7 +32,7 @@ const methods = {
     return `${baseUrl}/users/${userId}/purview_users?scope=canInstruct`
   },
   availabilities(userId, { startDate, endDate }) {
-    return `${baseUrl}/users/${userId}/availabilities?state_date=${startDate}&end_date=${endDate}`
+    return `${baseUrl}/users/${userId}/availabilities?start_date=${startDate}&end_date=${endDate}`
   },
   userAvailabilities(userId, { startDate, endDate }) {
     return `${baseUrl}/users/${userId}/purview_availabilities?start_date=${startDate}&end_date=${endDate}&key_by=user_id`
