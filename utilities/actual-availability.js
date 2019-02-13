11278e55443c572fb2911f9b19e429d58bc894da
Kyle Coberly <kyle.coberly@gmail.com>
2019-02-13 16:41:26 -0700

    Resolves features/SIM-1084

--- utilities/actual-availability.js
+++ utilities/actual-availability.js
@@ -39,12 +39,12 @@ function getEnoughUsers(_, days, bookings, statedAvailabilities, count) {
     return flow([
       omitWithKey((users, date) => bookings[date]),
       mapValuesWithKey((users, date) => {
-        return omitWithKey((availabilities, user) => bookings[date][user])(users)
+        return omitWithKey((availabilities, user) => get('[date][user]')(bookings))
       }),
       mapValuesWithKey((users, date) => {
         return mapValuesWithKey((availabilities, user) => {
-          return bookings[date][user]
-            ? difference(bookings[date][user] || [])(availabilities)
+          return get('[date][user]')(bookings)
+            ? difference(get('[date][user]')(bookings) || [])(availabilities)
             : availabilities
         })(users)
       }),
