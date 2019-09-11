6e107139c2f0b8f540fd5920e63ca57b98b7e8da
sonja <sonjita.chacon@gmail.com>
2019-09-11 13:43:19 -0600

    update 2

--- components/FilterBy.vue
+++ components/FilterBy.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="sim-filter sim-accordion" :class="{ active: shouldBeActive, open: isOpen }">
 
-    <!-- <div class="sim-filter--header sim-accordion--label" @click="toggleOpenList">{{ label }}</div>
-    <div v-if="filterDepartmentByAlphaNum" class="filter filter--alpha">
+    <div class="sim-filter--header sim-accordion--label" @click="toggleOpenList">{{ label }}</div>
+    <div v-if="filterDepartment" class="filter filter--alpha">
       <ul class="list flex-center">
         <li
           v-for="item in filterList"
@@ -10,7 +10,7 @@
           <a href="#0" @click="filterDepartment">{{ item.title }}</a>
         </li>
       </ul>
-    </div> -->
+    </div>
 
     <SimDatalist v-if="!this.shouldShowAutocomplete" :items="list" :animate="true" class="sim-filter--items sim-accordion--items">
       <li slot="static-before" key="static-before" class="static system-echo FIXME-generic-classes" v-if="showSystemEcho">
