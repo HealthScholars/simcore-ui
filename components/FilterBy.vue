101a2db17f6ce1e6742ae0fb032f0cc9585573b9
sonja <sonjita.chacon@gmail.com>
2019-09-12 18:12:50 -0600

    Revert "sorting"
    This reverts commit db222bdfdd6bfb5c3e46861d32cfc17aa6b27c81.

--- components/FilterBy.vue
+++ components/FilterBy.vue
@@ -2,16 +2,16 @@
   <div class="sim-filter sim-accordion" :class="{ active: shouldBeActive, open: isOpen }">
     
     <div class="sim-filter--header sim-accordion--label" @click="toggleOpenList">{{ label }}</div>
-      <div v-if="filterDepartmentByAlphaNum" class="filter filter--alpha">
-          <ul class="list">
-              <li
-              v-for="item in filterList"
-              :key="item.title">
-              <a href="#0" @click="filterDepartment">{{ item.title }}</a>
-              </li>
-          </ul>
-      </div>
-   
+    <div v-if="filterDepartmentByAlphaNum" class="filter filter--alpha">
+      <ul class="list">
+        <li
+          v-for="item in filterList"
+          :key="item.title">
+          <a href="#0" @click="filterDepartment">{{ item.title }}</a>
+        </li>
+      </ul>
+    </div>
+
     <SimDatalist v-if="!this.shouldShowAutocomplete" :items="list" :animate="true" class="sim-filter--items sim-accordion--items">
         <li slot="static-before" key="static-before" class="static system-echo FIXME-generic-classes" v-if="showSystemEcho">
           {{ systemEcho }}
@@ -66,7 +66,7 @@
         type: Number,
         default: 15,
       },
-      filterDepartmentByAlphaNum:{
+      filterDepartmentByAlphaNum :{
         type: Boolean
       },
     },
@@ -119,7 +119,7 @@
           this.selectedItems.splice(this.selectedItems.indexOf(id), 1)
         }
       },
-      filterDepartment() {
+      filterDepartmentByAlphaNum() {
         return true
       }
     },
@@ -135,3 +135,12 @@
 <style lang="scss">
   @import '../styles/filter-by';
 </style>
+    <div v-if="filterDepartmentByAlphaNum" class="filter filter--alpha">
+      <ul class="list">
+        <li
+          v-for="item in filterList"
+          :key="item.title">
+          <a href="#0" @click="filterDepartmentByAlphaNum">{{ item.title }}</a>
+        </li>
+      </ul>
+    </div>
