07346b6b526869d62e3b692f30ea792ce80bd1b6
sonja <sonjita.chacon@gmail.com>
2019-09-16 14:03:19 -0600

    checking pagintion

--- components/FilterBy.vue
+++ components/FilterBy.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <div class="sim-filter sim-accordion" :class="{ active: shouldBeActive, open: isOpen }">
-    
+  
+  <section>
     <div class="sim-filter--header sim-accordion--label" @click="toggleOpenList">{{ label }}</div>
       <div v-if="filterDepartmentByAlphaNum" class="filter filter--alpha sim-accordion--items">
         <ul class="list">
@@ -12,6 +13,17 @@
         </ul>
       </div>
 
+      <div v-if="filterDepartmentByAlphaNum"class="flex-row flex-align-center sim-filter--header">
+          <button @click="prevPage" class="link">
+              <SimIconText data-testid="previousButton" icon="fa-arrow-left fa-fw"></SimIconText>
+          </button>
+          <span class="nowrap"></span>
+          <button @click="nextPage" class="link">
+              <SimIconText data-testid="nextButton" icon="fa-arrow-right fa-fw"></SimIconText>
+          </button>
+      </div>
+  </section>
+  
     <SimDatalist v-if="!this.shouldShowAutocomplete" :items="list" :animate="true" class="sim-filter--items sim-accordion--items">
         <li slot="static-before" key="static-before" class="static system-echo FIXME-generic-classes" v-if="showSystemEcho">
           {{ systemEcho }}
