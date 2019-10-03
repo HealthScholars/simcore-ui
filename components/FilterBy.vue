048baf9cf516f81452249af996e7eb820b1d6e54
sonja <sonjita.chacon@gmail.com>
2019-10-03 12:01:07 -0600

    $store issues

--- components/FilterBy.vue
+++ components/FilterBy.vue
@@ -113,7 +113,7 @@ export default {
       return this.list.length >= this.autocompleteThreshold;
     },
     getDepartmentsByLetter() {
-      return $store.getters.paginatedDepartments;
+      return this.$store.getters.paginatedDepartments;
     }
   },
   methods: {
