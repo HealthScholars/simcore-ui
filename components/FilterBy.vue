53964b31b7d0ab26f1504860160c559e282d0576
sonja <sonjita.chacon@gmail.com>
2019-10-03 16:27:22 -0600

    ..

--- components/FilterBy.vue
+++ components/FilterBy.vue
@@ -117,7 +117,7 @@ export default {
     }
   },
   mounted() {
-    console.log(getDepartmentsByLetter)
+    console.log(this.$store.getters.paginatedDepartments)
   },
   methods: {
     toggleOpenList() {
